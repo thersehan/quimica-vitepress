5b01b1489a6a571986fb4f211001eb7389c77c94
Juliano <juliano11.goncalves@gmail.com>
2025-07-31 19:42:04 -0300

    fix: base config

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
   cleanUrls: true,
   title: "Apostila",
   description: "Projeto de Iniciação Científica",
-  base: /quimica-vitepress/,
+  base: "/quimica-vitepress/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     logo: "logo.png",
