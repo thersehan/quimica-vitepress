b39ee04f0455194ffafb6eff01a6c8be3c8623f0
Juliano <juliano11.goncalves@gmail.com>
2025-08-03 16:48:17 -0300

    feat: adiciona demonstração konva na aba laboratório

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,6 +4,8 @@ import DefaultTheme from "vitepress/theme";
 import MyComponent from "./components/MyComponent.vue";
 import "./style.css";
 
+import VueKonva from "vue-konva";
+
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
@@ -14,5 +16,6 @@ export default {
   },
   enhanceApp({ app, router, siteData }) {
     app.component("MyComponent", MyComponent);
+    app.use(VueKonva);
   },
 };
--- docs/lab.md
+++ docs/lab.md
@@ -3,6 +3,4 @@ layout: page
 sidebar: false
 ---
 
-Isso é um teste de inserção de um componente `.vue` dentro de uma página `markdown`.
-
 <MyComponent />
