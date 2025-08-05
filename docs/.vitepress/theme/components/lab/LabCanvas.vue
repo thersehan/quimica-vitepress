df4e028c46dc1fd821974215cf7ca2a50988dd51
Juliano <juliano11.goncalves@gmail.com>
2025-08-05 16:17:08 -0300

    test: átomo arrastável

--- docs/.vitepress/theme/components/lab/LabCanvas.vue
+++ docs/.vitepress/theme/components/lab/LabCanvas.vue
@@ -1,37 +1,19 @@
 <template>
   <v-stage ref="stage" :config="stageSize">
     <v-layer ref="layer">
-      <v-text
-        @dragstart="handleDragStart"
-        @dragend="handleDragEnd"
-        :config="{
-          text: 'Draggable Text',
-          x: 50,
-          y: 50,
-          draggable: true,
-          fill: isDragging ? 'green' : 'black',
-        }"
-      />
+      <Atom />
     </v-layer>
   </v-stage>
 </template>
 
 <script setup>
 import { ref, onBeforeMount } from "vue";
+import Atom from "./Atom.vue";
 
 const stageSize = ref({ width: 0, height: 0 });
-const isDragging = ref(false);
 
 onBeforeMount(() => {
   stageSize.value.width = window.innerWidth;
   stageSize.value.height = window.innerHeight;
 });
-
-function handleDragStart() {
-  isDragging.value = true;
-}
-
-function handleDragEnd() {
-  isDragging.value = false;
-}
 </script>
