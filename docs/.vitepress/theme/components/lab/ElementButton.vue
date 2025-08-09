808f2df8a1c143da9f917fa14156631f66d6e948
Juliano <juliano11.goncalves@gmail.com>
2025-08-09 09:20:35 -0300

    feat: atom add demo

---
+++ docs/.vitepress/theme/components/lab/ElementButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <Atom :config="elementConfig" @click="$emit('buttonClick')" />
+</template>
+
+<script setup>
+import Atom from "./Atom.vue";
+
+const elementConfig = {
+  x: 75,
+  y: 75,
+  radius: 20,
+  fill: "red",
+  stroke: "black",
+  strokeWidth: 2,
+  draggable: false,
+};
+</script>
--- docs/.vitepress/theme/components/lab/LabCanvas.vue
+++ docs/.vitepress/theme/components/lab/LabCanvas.vue
@@ -1,7 +1,10 @@
 <template>
   <v-stage ref="stage" :config="stageSize">
-    <v-layer ref="layer">
-      <Atom />
+    <v-layer ref="button-layer">
+      <ElementButton @button-click="addAtom" />
+    </v-layer>
+    <v-layer ref="atoms-layer">
+      <Atom v-for="atom of atoms" />
     </v-layer>
   </v-stage>
 </template>
@@ -9,8 +12,17 @@
 <script setup>
 import { ref, onBeforeMount } from "vue";
 import Atom from "./Atom.vue";
+import ElementButton from "./ElementButton.vue";
 
 const stageSize = ref({ width: 0, height: 0 });
+
+// const elements = ref({});
+
+const atoms = ref(0);
+
+function addAtom() {
+  atoms.value++;
+}
 
 onBeforeMount(() => {
   stageSize.value.width = window.innerWidth;
